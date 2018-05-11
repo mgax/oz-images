f021d95d2db72b721966f958370b5d564832de64
Alex Morega <alex@grep.ro>
2018-05-11 23:48:06 +0300

    reuse thumbnails

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as child_process from "child_process";
+import * as fs from "fs";
 
 const app: express.Application = express();
 
@@ -9,11 +10,17 @@ function greeter(person: string) {
 
 app.get('/', (req, res) => res.send(greeter('jane')));
 
+function randomNumber() {
+  return Math.ceil(Math.random() * 100000000);
+}
+
 app.get('/image/:name', (req, res) => {
   let root = app.get('oz-image-path');
   let name = req.params['name'];
   let original = root + '/' + name;
   let size = req.query.size;
+
+  // TODO replace all fs calls with async versions
 
   if(size) {
     if(! size.match(/^(\d+)x(\d+)$/)) {
@@ -22,15 +29,27 @@ app.get('/image/:name', (req, res) => {
     }
 
     // TODO limit thumbnail size
-    // TODO reuse thumbnails
 
-    let thumb = root + '/' + size + '-' + name;
+    let thumbDir = root + '/thumbs';
+    let thumb = thumbDir + '/' + size + '-' + name;
+
+    if(fs.existsSync(thumb)) {
+      res.sendFile(thumb);
+      return;
+    }
+
+    if(! fs.existsSync(thumbDir)) {
+      fs.mkdirSync(thumbDir);
+    }
+
+    let thumbTmp = thumb + '.' + randomNumber();
     let convert = child_process.spawn(
       'convert',
-      [original, '-thumbnail', size, thumb],
+      [original, '-thumbnail', size, thumbTmp],
     );
     // TODO error handling on spawn and close
     convert.on('close', () => {
+      fs.renameSync(thumbTmp, thumb);
       res.sendFile(thumb);
     })
   }
